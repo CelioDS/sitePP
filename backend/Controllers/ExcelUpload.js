2d733c6e983ae812cd9a589f2e87000eb4e0dad1
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2026-02-01 11:22:05 -0300

    Update ExcelUpload.js

--- backend/Controllers/ExcelUpload.js
+++ backend/Controllers/ExcelUpload.js
@@ -1,32 +1,21 @@
-import { dataBase } from "../DataBase/dataBase.js";
-import dotenv from "dotenv";
-import { format } from "date-fns";
-import { fromZonedTime } from "date-fns-tz";
-import multer from "multer";
-import XLSX from "xlsx";
-import fs from "fs";
-
-dotenv.config(); // <-- Carrega o arquivo .env
-
 export const upload = multer({
-  dest: "uploads/",
+  storage: multer.memoryStorage(),
 });
-
-
-/**___________________________LojaPropria___________________________**/
 
 export const setExcel = async (req, res) => {
   try {
     const login = req.headers.login;
+
     const TODAY = format(
       fromZonedTime(new Date(), "America/Sao_Paulo"),
-      "yyyy-MM-dd-HH-mm",
+      "yyyy-MM-dd-HH-mm"
     );
+
     if (!req.file) {
       return res.status(400).json({ error: "Arquivo não enviado" });
     }
 
-    const workbook = XLSX.readFile(req.file.path);
+    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
 
@@ -52,13 +41,6 @@ export const setExcel = async (req, res) => {
       login,
     ]);
 
-    if (rows.length < values.length) {
-      return res.status(400).json({ error: "Planilha com colunas faltando" });
-    }
-    if (rows.length > values.length) {
-      return res.status(400).json({ error: "Planilha com colunas a mais" });
-    }
-
     const sql = `
       INSERT INTO LP
       (ANOMES, CANAL, COLABORADOR, LOGIN_CLARO, COMTA, CABEAMENTO, LOGIN_NET, LOJA, CIDADE, COORDENADOR, STATUS, DATA_ATUALIZACAO, LOGIN_ATUALIZACAO)
@@ -66,8 +48,6 @@ export const setExcel = async (req, res) => {
     `;
 
     await dataBase.query(sql, [values]);
-
-    fs.unlinkSync(req.file.path);
 
     return res.json({
       message: "Arquivo importado com sucesso",
