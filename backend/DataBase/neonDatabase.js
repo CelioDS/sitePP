287cf554083274f7abf9d7974dc9a9aa0a4caff5
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2026-04-25 23:26:57 -0300

    Update neonDatabase.js

--- backend/DataBase/neonDatabase.js
+++ backend/DataBase/neonDatabase.js
@@ -3,19 +3,11 @@ import dotenv from "dotenv"; // Corrigido: dotenv
 
 dotenv.config(); // Corrigido: dotenv
 
-if (!process.env.NEON_DATABASE_URL) {
-  throw new Error(
-    "🚨 FATAL: NEON_DATABASE_URL não definida nos Environment Variables da Vercel!",
-  );
-}
-
 export const neonDB = new pg.Pool({
-  connectionString: process.env.NEON_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-  // Adicione isso para evitar que ele tente o padrão local
-  host: process.env.NEON_DATABASE_URL.split("@")[1]?.split("/")[0] || undefined,
 });
 
 neonDB.query("SELECT NOW()", (err, res) => {
